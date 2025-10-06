bb3910de415369b68cf6fcdd65cb9d880c46524a
mohamedAbdelazim <mohamedabdelazim654@gmail.com>
2025-10-06 21:17:47 +0300

    edit project

--- server.ts
+++ server.ts
@@ -1,24 +1,25 @@
+
 import "reflect-metadata";
 import { useExpressServer } from "routing-controllers";
 import compression from "compression";
 import cors from "cors";
 import dotenv from "dotenv";
-import { ErrorHandler } from "./middlewares/ErrorHandler";
+import { ErrorHandler } from "./src/middlewares/ErrorHandler";
 dotenv.config();
 import express from "express";
-import { createValidationMiddleware } from "./middlewares/validation";
-import { userControllers } from "./Modules/users/user.controllers";
-import { roleControllers } from "./Modules/roles/role.controllers";
-import { serviceController } from "./Modules/service/service.controlers";
-import { specialtyControllers } from "./Modules/Specialist/specialist.controllers";
-import { invoiceControllers } from "./Modules/invoice/invoice.controllers";
-import { doctorControllers } from "./Modules/doctor/doctor.controllers";
-import { patientController } from "./Modules/patient/patient.controllers";
-import { scheduleControllers } from "./Modules/schedule/schedule.controllers";
-import { appointmentController } from "./Modules/appointment/appoientment.controllers";
-import { visitController } from "./Modules/visit/visit.controllers";
-import { PermissionController } from "./Modules/permission/seeder";
-import { searchControllers } from "./Modules/searchEngine/searchEngine";
+import { createValidationMiddleware } from "./src/middlewares/validation";
+import { userControllers } from "./src/Modules/users/user.controllers";
+import { roleControllers } from "./src/Modules/roles/role.controllers";
+import { serviceController } from "./src/Modules/service/service.controlers";
+import { specialtyControllers } from "./src/Modules/Specialist/specialist.controllers";
+import { invoiceControllers } from "./src/Modules/invoice/invoice.controllers";
+import { doctorControllers } from "./src/Modules/doctor/doctor.controllers";
+import { patientController } from "./src/Modules/patient/patient.controllers";
+import { scheduleControllers } from "./src/Modules/schedule/schedule.controllers";
+import { appointmentController } from "./src/Modules/appointment/appoientment.controllers";
+import { visitController } from "./src/Modules/visit/visit.controllers";
+import { PermissionController } from "./src/Modules/permission/seeder";
+import { searchControllers } from "./src/Modules/searchEngine/searchEngine";
 
 const app = express();
 
